56118ac2656946e2de33228d16ccf06a850558e5
chewie <quentin.sanjose@y-nov.com>
2019-01-08 22:29:45 +0100

    sans les js

--- app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Chien_1 = require("./app/Chien");
-let connard = new Chien_1.Chien.Enumerer();
-connard.enumere();
